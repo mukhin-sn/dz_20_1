bdd833fffb8f3ce8166bc0278446dac293a13e8e
mukhin-sn <senikomu@gmail.com>
2024-01-29 02:37:30 +0700

    error correction

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.global_settings import MEDIA_ROOT
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    # path('', include('main.urls')),
+] + static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
