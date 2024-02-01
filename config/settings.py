00da9a365038c0164c3c3ca644f272f4e1460635
Sergey Mukhin <senikomu@gmail.com>
2024-02-01 22:08:31 +0700

    error correction

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import main.apps
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "main",
+    # "main",
+    "main.apps.MainConfig",
 ]
 
 MIDDLEWARE = [
@@ -122,11 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = (
+    BASE_DIR / "static",
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- main/models.py
+++ main/models.py
@@ -3,27 +3,10 @@ from django.db import models
 NULLABLE = {'null': True, 'blank': True}
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='наименование')
-    description = models.CharField(max_length=200, verbose_name='описание')
-    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
-    category = models.CharField(max_length=50, verbose_name='категория')
-    price_for_one = models.IntegerField(verbose_name='цена за штуку')
-    date_of_creation = models.DateField(verbose_name='дата создания')
-    last_modified_date = models.DateField(verbose_name='дата последнего изменения')
-
-    def __str__(self):
-        return f'{self.name} {self.description}'
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
     description = models.CharField(max_length=200, verbose_name='описание')
-    created_at = models.DateField(verbose_name='дата создания', **NULLABLE)
+    # created_at = models.DateField(verbose_name='дата создания', **NULLABLE)
 
     def __str__(self):
         return f'{self.name} {self.description}'
@@ -31,3 +14,20 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.CharField(max_length=200, verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, max_length=50, verbose_name='категория')
+    price_for_one = models.IntegerField(verbose_name='цена за штуку')
+    date_of_creation = models.DateField(verbose_name='дата создания', **NULLABLE)
+    last_modified_date = models.DateField(verbose_name='дата последнего изменения', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
